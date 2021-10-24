609595bc74b3268b81af841618746ab5bdfdef4b
FillipiGS <40797131+FillipiGS@users.noreply.github.com>
2021-10-24 17:59:44 -0300

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
     class Hello {  
        public static void main(String args[]) {  
-          System.out.println("Hello World!");     
+           for (int i = 0; i <= 13; i++) {
+               System.out.println("Hello World " + i);   
+           }
+            
        }  
     }  
